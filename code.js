156a552cd47acda1781bf5c3a57093016a7c2921
GuiPrazeres <ricardoprazeress@gmail.com>
2022-10-31 14:05:16 -0300

    Importacao de array de objetos literais

--- code.js
+++ code.js
@@ -1,13 +1,19 @@
 import modificador from './modificador.js';
+import ingredientes from './ingredientes.js';
 
-console.log(modificador);
+//console.log(modificador);
 
-var ingredientes = [ 'mel', 'água', 'sal', 'mostarda'];
-var resultadoCapitalizado = modificador.capitalizar(ingredientes);
-var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado);
-var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
+//var ingredientes = [ 'mel', 'água', 'sal', 'mostarda'];
+// var resultadoCapitalizado = modificador.capitalizar(ingredientes);
+// var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado);
+// var resultadoCaixaAlta = modificador.caixaAlta(ingredientes)
 
-console.log(resultadoCapitalizado);
-console.log(resultadoOrdenacao);
-console.log(resultadoCaixaAlta);
+// console.log(resultadoCapitalizado);
+// console.log(resultadoOrdenacao);
+// console.log(resultadoCaixaAlta);
 
+console.log(ingredientes);
+console.log(typeof ingredientes);
+console.log(Array.isArray(ingredientes));
+console.log(ingredientes[0]);
+console.log(ingredientes[1].nome);
---
+++ ingredientes.js
@@ -0,0 +1,10 @@
+var ingredientes = [
+	{nome:'mel',
+	valor: 2.5},
+	{nome: 'tomate',
+	valor: 3.5},
+	{nome:'batata palha',
+	valor: 5}
+];
+
+export default ingredientes;
